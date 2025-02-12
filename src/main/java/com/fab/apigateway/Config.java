91d1669489c30b6df4b64502c1edd1a776fb79d9
fbenaglia <fbenaglia@hotmail.com>
2025-02-12 12:52:21 -0300

    configuration routes

--- src/main/java/com/fab/apigateway/Config.java
+++ src/main/java/com/fab/apigateway/Config.java
@@ -11,15 +11,12 @@ public class Config {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route("task",
-						r -> r.path("/api/task/**", "/auth/callback", "/auth/url", "/api/task/files/**").and()
-								.method("GET", "POST", "PUT", "DELETE").uri("lb://task"))
-				.route("security", r -> r.path("/auth/authenticate", "/userData/**").and().method("POST", "GET").and()
-						.uri("lb://security"))
+				.route("reactivehub",
+						r -> r.path("/climateData/**").and()
+								.uri("lb://reactivehub"))
 				.build();
 
-//http://security	ok con post y get  (a veces falla por razones desconocidas, podria se eureka el responsable)
-//http://task ok con todos los get task auth y files. post a api/task no funciona con jwt no se puede probar
+
 
 	}
 
